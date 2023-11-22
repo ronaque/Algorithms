09d266a1c7cf6b41984f2b0f5230750fc3c72f92
Isaque <isaque.bm9@gmail.com>
2023-11-21 23:58:06 -0300

    feat: created frame to add and remove nodes and add edges

--- data_structures/graphs/graph.py
+++ data_structures/graphs/graph.py
@@ -1,10 +1,13 @@
 from data_structures.graphs.node import Node
 
 class Graph:
-    def __init__(self, nodes=None):
+    def __init__(self, nodes=None, edges=None):
+        if edges is None:
+            edges = []
         if nodes is None:
-            nodes = list()
+            nodes = []
         self.nodes = nodes
+        self.edges = edges
 
     def add_new_node(self):
         """
@@ -13,19 +16,61 @@ class Graph:
         -------
 
         """
-        last_id = 0 if not self.nodes else self.nodes[-1].id_no + 1
+        last_id = 0 if not self.nodes else self.nodes[-1].id_node + 1
         node = Node(last_id)
         self.nodes.append(node)
 
-    def add_node(self, no):
-        self.nodes.append(no)
+    def get_node_by_id(self, id_node) -> Node | None:
+        """
+        Get a node by its id, if the node don't exist, return None
+        Parameters
+        ----------
+        id_node: Id of the node
 
-    def get_node_by_id(self, id_no) -> Node | None:
+        Returns
+        -------
+        Node | None
+        """
         for no in self.nodes:
-            if no.id_no == id_no:
+            if no.id_node == id_node:
                 return no
         return None
 
-    def remove_node(self, no):
-        self.nodes.remove(no)
+    def remove_node(self, node):
+        self.nodes.remove(node)
 
+    def add_edge(self, id_node1, id_node2):
+        """
+        Add an edge between two nodes, using their ids
+        Parameters
+        ----------
+        id_node1: Id of the first node
+        id_node2: Id of the second node
+
+        Returns
+        -------
+
+        """
+        node1 = self.get_node_by_id(id_node1)
+        node2 = self.get_node_by_id(id_node2)
+        node1.add_adjascent(node2)
+        node2.add_adjascent(node1)
+        self.edges.append((node1, node2))
+
+    def check_edge_exist(self, id_node1, id_node2):
+        """
+        Check if an edge exists between two nodes, using their ids
+        Parameters
+        ----------
+        id_node1: Id of the first node
+        id_node2: Id of the second node
+
+        Returns
+        -------
+        bool
+        """
+        node1 = self.get_node_by_id(id_node1)
+        node2 = self.get_node_by_id(id_node2)
+        if (node1, node2) in self.edges or (node2, node1) in self.edges:
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from utils.cli import execute_factorization, execute_graphs
+from utils.tkinter import create_graph_with_tkinter
 
 if __name__ == '__main__':
-    command = int(input("Choose what you want to execute:"
-                        "\n0 - Exit"
-                        "\n1 - Graph Algorithms"
-                        "\n2 - Factorization Algoriths"
-                        "\n"))
-
-    while command != 0:
-        match command:
-            case 1:
-                execute_graphs()
-            case 2:
-                execute_factorization()
-
-        command = int(input("Choose what you want to execute:"
-                            "\n0 - Exit"
-                            "\n1 - Graph Algorithms"
-                            "\n2 - Factorization Algoriths"
-                            "\n"))
-
-    print("Fim da execução")
+    create_graph_with_tkinter()
+    # command = int(input("Choose what you want to execute:"
+    #                     "\n0 - Exit"
+    #                     "\n1 - Graph Algorithms"
+    #                     "\n2 - Factorization Algoriths"
+    #                     "\n"))
+    #
+    # while command != 0:
+    #     match command:
+    #         case 1:
+    #             execute_graphs()
+    #         case 2:
+    #             execute_factorization()
+    #
+    #     command = int(input("Choose what you want to execute:"
+    #                         "\n0 - Exit"
+    #                         "\n1 - Graph Algorithms"
+    #                         "\n2 - Factorization Algoriths"
+    #                         "\n"))
+    #
+    # print("Fim da execução")
